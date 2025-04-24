9332a7ed14b9d805668a588882eb186c186f4576
MarceIsid <marc.segovia@duocuc.cl>
2025-04-24 18:04:36 -0400

    Update llamar.js

--- llamar.js
+++ llamar.js
@@ -2,7 +2,7 @@ const twilio = require('twilio');
 
 // Reemplaza con tus credenciales reales de Twilio
 const accountSid = 'AC414f4673f20f1148dc8c8a20b1d8b935';
-const authToken = '136b4acf070a4ce3a25b6e2026685c90';
+const authToken = '020825ded25998fde64eb305f96247a6';
 
 const client = new twilio(accountSid, authToken);
 
